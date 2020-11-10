7c984786e63c922554e71076b421054daf1ee0d8
clearlyyy <73618253+clearlyyy@users.noreply.github.com>
2020-11-10 13:21:45 -0800

    Updated config.lua
    yessir

--- config.lua
+++ config.lua
@@ -1,8 +1,15 @@
 Config                            = {}
 
 Config.DrawDistance               = 100.0
-Config.MarkerType                 = 1
+Config.MarkerType                 = 29
 Config.MarkerSize                 = { x = 1.5, y = 1.5, z = 0.5 }
-Config.MarkerColor                = { r = 50, g = 50, b = 204 }
+Config.MarkerColor                = { r = 4, g = 255, b = 0 }
+
+-- CHANGE TAXRATE FOR MONEYWASH, EX with 0.5 taxrate, 100,000 black_money will equal 50,000 cash.
+-- EX> With 0.7 taxrate 100,000 black_money will equal 70,000 cash.
+Config.Tax                        = 0.8
+
+-- Please see client.lua and go to line 31 to change/edit MoneyWash Locations.
+
 
 Config.Locale = 'en'

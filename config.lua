5389fb8e13ef0d4f5de5e7041f209322f97b13f9
clearlyyy <73618253+clearlyyy@users.noreply.github.com>
2020-11-10 14:51:45 -0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,9 +5,17 @@ Config.MarkerType                 = 29
 Config.MarkerSize                 = { x = 1.5, y = 1.5, z = 0.5 }
 Config.MarkerColor                = { r = 4, g = 255, b = 0 }
 
+
 -- CHANGE TAXRATE FOR MONEYWASH, EX with 0.5 taxrate, 100,000 black_money will equal 50,000 cash.
 -- EX> With 0.7 taxrate 100,000 black_money will equal 70,000 cash.
 Config.Tax                        = 0.8
+
+-- Choose whatever coords you want, these coords are for the laundry mat
+Config.Location = { 
+    X = 1122.724,
+    Y = -3194.481,
+    Z = -40.39858
+}
 
 -- Please see client.lua and go to line 31 to change/edit MoneyWash Locations.
 
